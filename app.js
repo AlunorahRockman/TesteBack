62ea4c4ec1501d8d2555c58406e66ae5461611c6
AlunorahRockman <ainatolotriniavo.a@zurcher.edu.mg>
2023-10-12 13:33:05 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from "express"
+import database from "./database/database.js"
+import router from "./root/root.js"
+import cors from "cors"
+
+const app = express()
+
+app.use(cors({
+    origin:'*'
+}))
+app.use(express.json)
+app.use(router)
+
+database.sync()
+.then((console.log('Connexion à la bdd')))
+.catch(error => console.log(error)) 
+
+app.listen(4000, () => console.log('Port 4000')) 
